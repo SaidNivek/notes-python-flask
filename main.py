22b9736fe412912417b5abb8f3f20d123c4e41a2
SaidNivek <kevin.vi.dias@gmail.com>
2023-02-27 21:55:16 -0500

    Setup a Blueprint for the flask app

---
+++ main.py
@@ -0,0 +1,9 @@
+# Website is a Python package because of the __init__.py folder that we added to it
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    # debug = True means that the web server will update and run changes when changes are made
+    # Turn this off when it's in production
+    app.run(debug = True)
---
+++ website/views.py
@@ -0,0 +1,5 @@
+# Store the standard routes for our websites, where the user can navigate to
+from flask import Blueprint
+
+# This file is a blueprint of our application
+views = Blueprint('views', __name__)
