d30cef7c52faaf2a19522bf87511da1190d764ab
SaidNivek <kevin.vi.dias@gmail.com>
2023-02-27 21:59:52 -0500

    Begin registering views and auth

--- website/__init__.py
+++ website/__init__.py
@@ -5,4 +5,11 @@ def create_app():
     # Will encrypt/secure the cookies and session associated with the website
     app.config['SECRET_KEY'] = 'apples bananas cats dogs puppies smooches children'
 
+    from .views import views
+    from .auth import auth
+
+    # We must register the imported views to their url_prefix, which will tell the website where to go when that url is hit
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
--- website/views.py
+++ website/views.py
@@ -3,3 +3,8 @@ from flask import Blueprint
 
 # This file is a blueprint of our application
 views = Blueprint('views', __name__)
+
+# Whenever we go to the main page of our website (/), whatever is inside the home function will run
+@views.route('/')
+def home():
+    return "<h1>Test</h1>"
