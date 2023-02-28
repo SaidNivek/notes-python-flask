b8f8d4f8b5db695fa66adfd1bb13b766e5850a1a
SaidNivek <kevin.vi.dias@gmail.com>
2023-02-27 22:12:56 -0500

    Fixed error that would prevent app from showing in browser

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
+# Make sure there is no space between the == or it won't work
+if __name__=='__main__':
     # debug = True means that the web server will update and run changes when changes are made
     # Turn this off when it's in production
     app.run(debug = True)
