e1a0f2a4db6c066942e4159736d58a2378c58524
SaidNivek <kevin.vi.dias@gmail.com>
2023-02-28 22:25:35 -0500

    Hit blocker on running live server here

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 # Store the standard routes for our websites, where the user can navigate to
-from flask import Blueprint
+# Whenever we want to render a template, must import render_template function
+from flask import Blueprint, render_template
 
 # This file is a blueprint of our application
 views=Blueprint('views', __name__)
@@ -7,4 +8,5 @@ views=Blueprint('views', __name__)
 # Whenever we go to the main page of our website (/), whatever is inside the home function will run
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template('home.html')
+    # To render a template, we return the render_template function and then the name of the html template
