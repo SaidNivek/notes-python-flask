e16a2700deee2bbeb75ee94a731eb254ae56f0d2
SaidNivek <kevin.vi.dias@gmail.com>
2023-03-05 15:36:51 -0500

    Begin POST request for signup/login pages

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,12 @@
 # Store the standard routes for our websites, where the user can navigate to
-from flask import Blueprint, render_template
+# Need to import request at the top of our flask app so we can use it in the HHTP requests
+from flask import Blueprint, render_template, request
 
 # This file is a blueprint of our application, specifically for authentication
 auth=Blueprint('auth', __name__)
 
-@auth.route('/login')
+# In order for a route to be able to accept HTTP requests, we need to add them to the methods=[], passing in a string of the requests that we want that particular route to be able to handle
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
@@ -12,6 +14,6 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template('sign_up.html')
