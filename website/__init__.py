ad9e53601c6afdfc05279a7d1f84e689603c478c
SaidNivek <kevin.vi.dias@gmail.com>
2023-03-06 18:55:47 -0500

    Add SQLite and SQLAlchemy to init.py and connect the db to the app

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+# We need this for the SQL database that we are going to use for this project
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize a new database
+# This is the object we are going to use whenever we need to use a database for a user, for notes, etc.
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app=Flask(__name__)
     # Will encrypt/secure the cookies and session associated with the website
     app.config['SECRET_KEY'] = 'apples bananas cats dogs puppies smooches children'
+    # Our SQL Lite database is stored at the location {DB_NAME}
+    # This tells the app where we can find the database for this program
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # This gives the app to the database, so it knows what app is using it
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
