f0a09fda59e32cdfe144b0af1c2e1528b031fb10
SaidNivek <kevin.vi.dias@gmail.com>
2023-03-15 20:05:12 -0400

    Login functionality

--- website/auth.py
+++ website/auth.py
@@ -17,8 +17,27 @@ def login():
     # If we want to get the info sent from this form, we set data = request.form
     # When accessed inside of a route, the request variable will have the data that was sent in the request from within that route
     # By using request.form, we will be able to access the info that was in the form on that route
-    # data=request.form
-    # print(data)
+    # To log a user in, we have to get the data from the form 
+    if request.method == 'POST':
+        email=request.form.get('email')
+        password=request.form.get('password')
+
+        # the user will be set to the User from the database, getting a query, and then filtering by the email passed in, above
+        # .first() will return the first result, even though there should only be one
+        user = User.query.filter_by(email=email).first()
+        # if there is a found user, we need to check to see if the password is equal to the hash that we have stored for that user's password
+        if user:
+            # Use the check_password_hash, passing in the user's password from the database AND the password from the login form
+            # If the two match, then we can log the user in
+            if check_password_hash(user.password, password):
+                # If the passwords match, flash logged in successfully
+                flash('Logged in successfully!', category='success')
+            else:
+                flash('Incorrect email or password. Please try again.', category='error')
+        # If there is no user, let them know that there is an error
+        else: 
+            flash('Incorrect email or password. Please try again.', category='error')
+
     return render_template('login.html')
 
 @auth.route('/logout')
