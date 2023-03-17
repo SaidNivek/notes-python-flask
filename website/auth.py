0dc74d08fb407f4a9d64d3306641a80e321c2a2b
SaidNivek <kevin.vi.dias@gmail.com>
2023-03-16 22:09:39 -0400

    Imports from flask_login

--- website/auth.py
+++ website/auth.py
@@ -7,6 +7,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 # Import the db from __init__.py to be able to use it here
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 # This file is a blueprint of our application, specifically for authentication
 auth=Blueprint('auth', __name__)
