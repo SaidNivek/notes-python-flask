31f1157dd68ca6d0f7da97682b1d2984795587aa
SaidNivek <kevin.vi.dias@gmail.com>
2023-03-22 16:36:56 -0400

    Finish delete note function

--- website/views.py
+++ website/views.py
@@ -1,9 +1,11 @@
 # Store the standard routes for our websites, where the user can navigate to
 # Whenever we want to render a template, must import render_template function
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+# json is built into python - needed for the delete_note definition below
+import json
 
 # This file is a blueprint of our application
 views=Blueprint('views', __name__)
@@ -26,3 +28,21 @@ def home():
 
     return render_template('home.html', user=current_user)
     # To render a template, we return the render_template function and then the name of the html template
+
+@views.route('delete-note', methods=['POST'])
+def delete_note():
+    # The request data is a string
+    # the string is sent from the index.js, where the deleteNote function is defined
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    # Look for the note that has the Id of the note that is passed in
+    note = Note.query.get(noteId)
+    # If it exists, we can delete
+    if note:
+        # If the user who is signed in owns this note, then we will delete that note
+        # Prevents other users from deleting all of the notes from everyone else's account
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()            
+        # Return an empty response, since we do need to return something from these views   
+        return jsonify({})
